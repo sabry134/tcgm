13dd12fd5e16ee6e2b9cf80f945383d1c221a6fa
zacharie lacombe <zacharie.lacombe@epitech.eu>
2024-07-05 12:54:35 +0200

    :sparkles: adding new event to displace card and trying to add text to card

--- game_editor/src/card.cpp
+++ game_editor/src/card.cpp
@@ -1,34 +1,44 @@
 #include "card.hpp"
 #include <SOIL/SOIL.h>
 
-cardGraphique::cardGraphique(float x, float y, float width, float height) {
+cardGraphique::cardGraphique(float x, float y, float width, float height)
+{
     pos.x = x;
     pos.y = y;
     size.width = width;
     size.height = height;
 }
 
-position cardGraphique::get_pos() {
+position cardGraphique::get_pos()
+{
     return pos;
 };
 
-Size cardGraphique::get_size() {
+Size cardGraphique::get_size()
+{
     return size;
 };
 
-void cardGraphique::setTextures(const char* path) {
+void cardGraphique::setPos(position newPos)
+{
+    pos = newPos;
+};
+
+void cardGraphique::setTextures(const char *path)
+{
     textureID = SOIL_load_OGL_texture(
         path,
         SOIL_LOAD_AUTO,
         SOIL_CREATE_NEW_ID,
-        SOIL_FLAG_INVERT_Y
-    );
-    if (textureID == 0) {
+        SOIL_FLAG_INVERT_Y);
+    if (textureID == 0)
+    {
         std::cerr << "Failed to load texture 1." << std::endl;
         textureID = 0;
     }
 };
 
-GLuint cardGraphique::getTexture() {
-    return textureID;  
+GLuint cardGraphique::getTexture()
+{
+    return textureID;
 }
--- game_editor/src/card.hpp
+++ game_editor/src/card.hpp
@@ -1,34 +1,39 @@
 #ifndef CARD_H
 #define CARD_H
-#include <iostream> 
-#include <string> 
+#include <iostream>
+#include <string>
 #include <vector>
 #include <GL/glew.h>
 
-struct position {
+struct position
+{
     float x, y;
 };
 
-struct Size {
+struct Size
+{
     float width, height;
 };
 
-class cardGraphique {
+class cardGraphique
+{
 private:
     position pos; // Position of the rectangle
-    Size size; // Size of the rectangle
+    Size size;    // Size of the rectangle
     GLuint textureID = 0;
+
 public:
     cardGraphique(float px, float py, float width, float height);
     position get_pos();
     Size get_size();
     GLuint getTexture();
-    void setTextures(const char* path);
+    void setPos(position pos);
+    void setTextures(const char *path);
 };
 
-void addCard(std::vector<cardGraphique>& cards, GLuint vertexbuffer, GLuint uvbuffer, GLint useTextureLocation, GLint rectColorLocation, std::vector<GLfloat> vertexData, std::vector<GLfloat> uvData);
-GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path);
-GLuint loadTexture(const char* path);
+void addCard(std::vector<cardGraphique> &cards, GLuint vertexbuffer, GLuint uvbuffer, GLint useTextureLocation, GLint rectColorLocation, std::vector<GLfloat> vertexData, std::vector<GLfloat> uvData);
+GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
+GLuint loadTexture(const char *path);
 void drawBackground(GLuint textureID);
 std::vector<cardGraphique> generateCardGraphique(char *path);
 

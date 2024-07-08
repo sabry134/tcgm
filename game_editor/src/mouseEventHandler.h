db03ded0bc269af199737cf1683f3ec91955fb18
Van-Do <van-do.pham@epitech.eu>
2024-07-08 12:11:22 +0200

    :fire: blank state of main

--- game_editor/src/mouseEventHandler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "tcgmpch.h"
-#include "card.hpp"
-#include "Application.h"
-
-void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
-bool isInCard(std::vector<cardGraphique> cards, double xpos, double ypos);

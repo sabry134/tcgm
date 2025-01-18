eccc9decfbe0ddfdc84b8fb60627e2f0fcca4f7a
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2025-01-18 18:18:39 +0100

    :tada: setup react project

---
+++ src/front/tcgm-front/src/__tests__/Button.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Button from "./Button";
+
+test("renders a button with the given label", () => {
+  render(<Button label="Click Me" onClick={() => {}} />);
+  expect(screen.getByText("Click Me")).toBeInTheDocument();
+});
+
+test("calls the onClick handler when clicked", () => {
+  const onClickMock = jest.fn();
+  render(<Button label="Click Me" onClick={onClickMock} />);
+  fireEvent.click(screen.getByText("Click Me"));
+  expect(onClickMock).toHaveBeenCalledTimes(1);
+});
---
+++ src/front/tcgm-front/src/components/Button.jsx
@@ -0,0 +1,8 @@
+// src/components/Button.jsx
+import React from "react";
+
+const Button = ({ onClick, label }) => (
+  <button onClick={onClick}>{label}</button>
+);
+
+export default Button;

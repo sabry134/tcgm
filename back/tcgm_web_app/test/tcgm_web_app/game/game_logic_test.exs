58dbfb38f21c659e67be1efe4af031f593801f1b
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2025-02-05 22:17:53 +0100

    :fire: removed unused test

--- back/tcgm_web_app/test/tcgm_web_app/game/game_logic_test.exs
+++ back/tcgm_web_app/test/tcgm_web_app/game/game_logic_test.exs
@@ -32,15 +32,4 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     assert length(new_state.players["player1"]["field"]) == 1
     assert "Card X" in new_state.players["player1"]["field"]
   end
-
-  test "move_card moves card from source to destination", %{initial_state: state} do
-    state_with_card_in_hand = put_in(state.players["player1"]["hand"], ["Card Y"])
-    state_with_card_in_field = put_in(state_with_card_in_hand.players["player1"]["field"], ["Card Z"])
-    new_state = GameLogic.move_card_logic(state_with_card_in_field, "player1", "Card Y", "hand", "field")
-
-    assert length(new_state.players["player1"]["field"]) == 2
-    assert length(new_state.players["player1"]["hand"]) == 0
-    assert "Card Y" not in new_state.players["player1"]["hand"]
-    assert "Card Y" in new_state.players["player1"]["field"]
-  end
 end

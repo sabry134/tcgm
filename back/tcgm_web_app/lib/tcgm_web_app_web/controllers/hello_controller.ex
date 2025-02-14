dc0ca5ea34524a9963bdca132c5eec5bdb3f2895
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2025-02-14 14:43:45 +0100

    🔥 removed unused files in backend

--- back/tcgm_web_app/lib/tcgm_web_app_web/controllers/hello_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule TcgmWebAppWeb.HelloController do
-  use TcgmWebAppWeb, :controller
-
-  # The index action for the hello route
-  def index(conn, _params) do
-    json(conn, %{message: "Hello, World!"})
-  end
-end

4101eacc64ae1e1d41af7872a3dd5ea44c87216e
uvillanueva <91880049+uvillanueva@users.noreply.github.com>
2025-02-14 15:18:25 +0100

    :fire: removed unused too from config

--- back/tcgm_web_app/mix.exs
+++ back/tcgm_web_app/mix.exs
@@ -11,7 +11,6 @@ defmodule TcgmWebApp.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [
-        tool: ExCoveralls,
         ignore_modules: [
           TcgmWebApp.RoomRegistry,
           TcgmWebAppWeb.ChangesetJSON,

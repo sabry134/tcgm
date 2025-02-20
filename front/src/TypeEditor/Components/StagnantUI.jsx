df5b0ea8ed4dee3f87d86b5dd8874160b1384791
zacharie lacombe <zacharie.lacombe@epitech.eu>
2025-02-21 00:54:39 +0100

    :sparkles: adding toolbar to Type Editor

--- front/src/TypeEditor/Components/StagnantUI.jsx
+++ front/src/TypeEditor/Components/StagnantUI.jsx
@@ -2,6 +2,11 @@ import { Box } from '@mui/material'
 import { Component, React } from 'react'
 import './StagnantUI.css'
 import AddIcon from '@mui/icons-material/Add'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import PanToolAltIcon from '@mui/icons-material/PanToolAlt'
+
+import NearMeOutlinedIcon from '@mui/icons-material/NearMeOutlined'
 
 export class StagnantUI extends Component {
   constructor (props) {
@@ -11,8 +16,22 @@ export class StagnantUI extends Component {
   render () {
     return (
       <div className='stagnantCanva'>
-        <div className='addButton'>
-          <AddIcon />
+        <div className='toolBar'>
+          <div className='roundButton'>
+            <AddIcon />
+          </div>
+          <div className='roundButton'>
+            <PanToolAltIcon />
+          </div>
+          <div className='roundButton'>
+            <NearMeOutlinedIcon />
+          </div>
+          <div className='roundButton'>
+            <DeleteIcon />
+          </div>
+          <div className='roundButton'>
+            <ContentCopyIcon />
+          </div>
         </div>
       </div>
     )

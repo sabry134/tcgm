12a86a459913e94079c3746cd087fca3b32900af
root <benjamin.besnainou@epitech.eu>
2025-04-23 20:43:55 +0200

    :construction: refacto game logic file

--- back/tcgm_web_app/test/tcgm_web_app/game/game_logic_test.exs
+++ back/tcgm_web_app/test/tcgm_web_app/game/game_logic_test.exs
@@ -6,14 +6,14 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     initial_state = %{
       players: %{
         "player1" => %{
-          "hand" => %{},
-          "deck" =>  %{
-            "Card A" => %{"name" => "Lion","properties" => %{"attack" => 7, "defense" => 3}},
-            "Card B" => %{"name" => "Zombie","properties" => %{"attack" => 5, "defense" => 11}},
-            "Card C" => %{"name" => "Gobelin","properties" => %{"attack" => 8, "defense" => 4}}
-          },
-          "field" => %{},
-          "graveyard" => %{}
+          "hand" => [],
+          "deck" =>  [
+            %{"Card A" => %{"name" => "Lion","properties" => %{"attack" => 7, "defense" => 3}}},
+            %{"Card B" => %{"name" => "Zombie","properties" => %{"attack" => 5, "defense" => 11}}},
+            %{"Card C" => %{"name" => "Gobelin","properties" => %{"attack" => 8, "defense" => 4}}}
+          ],
+          "field" => [],
+          "graveyard" => []
         }
       },
       turn: "player1",
@@ -38,7 +38,7 @@ defmodule TcgmWebApp.Game.GameLogicTest do
   end
 
   test "draw_card with empty deck", %{initial_state: state} do
-    updated_state = update_in(state[:players]["player1"]["deck"], fn _deck -> %{} end)
+    updated_state = update_in(state[:players]["player1"]["deck"], fn _deck -> [] end)
     action_args = %{"amount" => 1}
     new_state = GameLogic.draw_card(updated_state, "player1", action_args)
 
@@ -49,33 +49,32 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     updated_state = update_in(state[:players]["player1"]["deck"], fn deck ->
       deck
       |> Enum.take(1)
-      |> Enum.into(%{})
     end)
     action_args = %{"amount" => 1}
     new_state = GameLogic.draw_card(updated_state, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["hand"]) == 1
-    assert map_size(new_state.players["player1"]["deck"]) == 0
-    assert Map.has_key?(new_state.players["player1"]["hand"], "Card A") == true
+    assert length(new_state.players["player1"]["hand"]) == 1
+    assert length(new_state.players["player1"]["deck"]) == 0
+    assert Enum.any?(new_state.players["player1"]["hand"], fn card -> Map.has_key?(card, "Card A") end) == true
   end
 
   test "draw_card moves card from deck to hand", %{initial_state: state} do
     action_args = %{"amount" => 1}
     new_state = GameLogic.draw_card(state, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["hand"]) == 1
-    assert map_size(new_state.players["player1"]["deck"]) == 2
-    assert Map.has_key?(new_state.players["player1"]["hand"], "Card A") == true
+    assert length(new_state.players["player1"]["hand"]) == 1
+    assert length(new_state.players["player1"]["deck"]) == 2
+    assert Enum.any?(new_state.players["player1"]["hand"], fn card -> Map.has_key?(card, "Card A") end) == true
   end
 
   test "draw_card draws more than one card", %{initial_state: state} do
     action_args = %{"amount" => 2}
     new_state = GameLogic.draw_card(state, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["hand"]) == 2
-    assert map_size(new_state.players["player1"]["deck"]) == 1
-    assert Map.has_key?(new_state.players["player1"]["hand"], "Card A") == true
-    assert Map.has_key?(new_state.players["player1"]["hand"], "Card B") == true
+    assert length(new_state.players["player1"]["hand"]) == 2
+    assert length(new_state.players["player1"]["deck"]) == 1
+    assert Enum.any?(new_state.players["player1"]["hand"], fn card -> Map.has_key?(card, "Card A") end) == true
+    assert Enum.any?(new_state.players["player1"]["hand"], fn card -> Map.has_key?(card, "Card B") end) == true
   end
 
   test "play_card with undefined player", %{initial_state: state} do
@@ -111,7 +110,7 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "properties" => %{"attack" => 3, "defense" => 9}
     }}
     action_args = %{"card" => card2}
-    state_with_card_in_hand = put_in(state.players["player1"]["hand"], card1)
+    state_with_card_in_hand = put_in(state.players["player1"]["hand"], [card1])
     new_state = GameLogic.play_card_logic(state_with_card_in_hand, "player1", action_args)
 
     assert new_state == {:error, "la carte n'existe pas"}
@@ -123,12 +122,13 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "properties" => %{"attack" => 15, "defense" => 10}
     }}
     action_args = %{"card" => card1}
-    state_with_card_in_hand = put_in(state.players["player1"]["hand"], card1)
+    state_with_card_in_hand = put_in(state.players["player1"]["hand"], [card1])
     new_state = GameLogic.play_card_logic(state_with_card_in_hand, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["hand"]) == 0
-    assert map_size(new_state.players["player1"]["field"]) == 1
-    assert Map.has_key?(new_state.players["player1"]["field"], "Card X") == true
+    assert length(new_state.players["player1"]["hand"]) == 0
+    assert length(new_state.players["player1"]["field"]) == 1
+    assert Enum.any?(new_state.players["player1"]["field"], fn card -> Map.has_key?(card, "Card X") end) == true
+
   end
 
   test "move_card with undefined player", %{initial_state: state} do
@@ -176,7 +176,7 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "properties" => %{"attack" => 3, "defense" => 9}
     }}
     action_args = %{"source" => "graveyard", "dest" => "deck", "card" => card2}
-    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], card1)
+    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], [card1])
     new_state = GameLogic.move_card_logic(state_with_card_in_graveyard, "player1", action_args)
 
     assert new_state == {:error, "la carte n'existe pas"}
@@ -188,7 +188,7 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "properties" => %{"attack" => 10, "defense" => 5}
     }}
     action_args = %{"source" => "graveyard", "dest" => "cemetery", "card" => card1}
-    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], card1)
+    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], [card1])
     new_state = GameLogic.move_card_logic(state_with_card_in_graveyard, "player1", action_args)
 
     assert new_state == {:error, "la destination n'existe pas"}
@@ -199,13 +199,14 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "name" => "Dragon",
       "properties" => %{"attack" => 10, "defense" => 5}
     }}
-    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], card1)
+    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], [card1])
     action_args = %{"source" => "graveyard", "dest" => "deck", "card" => card1}
     new_state = GameLogic.move_card_logic(state_with_card_in_graveyard, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["graveyard"]) == 0
-    assert map_size(new_state.players["player1"]["deck"]) == 4
-    assert Map.has_key?(new_state.players["player1"]["deck"], "Card Y") == true
+    assert length(new_state.players["player1"]["graveyard"]) == 0
+    assert length(new_state.players["player1"]["deck"]) == 4
+    assert Enum.any?(new_state.players["player1"]["deck"], fn card -> Map.has_key?(card, "Card Y") end) == true
+
   end
 
   test "move_card moves card from source to dest", %{initial_state: state} do
@@ -214,12 +215,12 @@ defmodule TcgmWebApp.Game.GameLogicTest do
       "properties" => %{"attack" => 10, "defense" => 5}
     }}
     action_args = %{"source" => "graveyard", "dest" => "hand", "card" => card1}
-    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], card1)
+    state_with_card_in_graveyard = put_in(state.players["player1"]["graveyard"], [card1])
     new_state = GameLogic.move_card_logic(state_with_card_in_graveyard, "player1", action_args)
 
-    assert map_size(new_state.players["player1"]["graveyard"]) == 0
-    assert map_size(new_state.players["player1"]["hand"]) == 1
-    assert Map.has_key?(new_state.players["player1"]["hand"], "Card Y") == true
+    assert length(new_state.players["player1"]["graveyard"]) == 0
+    assert length(new_state.players["player1"]["hand"]) == 1
+    assert Enum.any?(new_state.players["player1"]["hand"], fn card -> Map.has_key?(card,"Card Y") end) == true
   end
 
   test "update_values with undefined player", %{initial_state: state} do
@@ -284,7 +285,8 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     update_values_args = %{"location" => "graveyard", "key" => "attack", "value" => 7, "card" => "card1"}
     new_state = GameLogic.update_values_logic(state_with_card_in_graveyard, "player1", update_values_args)
 
-    assert new_state.players["player1"]["graveyard"]["card1"]["properties"]["attack"] == 7
+    card = Enum.find(new_state.players["player1"]["graveyard"], fn c -> Map.has_key?(c, "card1") end)
+    assert card["card1"]["properties"]["attack"] == 7
   end
 
   test "insert_card with undefined player", %{initial_state: state} do
@@ -328,7 +330,8 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     action_args = %{"location" => "graveyard", "card" => card1}
     state_with_card_in_graveyard = GameLogic.insert_card(state, "player1", action_args)
 
-    assert Map.has_key?(state_with_card_in_graveyard.players["player1"]["graveyard"], "Card Y") == true
+    assert Enum.any?(state_with_card_in_graveyard.players["player1"]["graveyard"], fn card -> Map.has_key?(card, "Card Y") end) == true
+
   end
 
   test "insert_card insert card into location with already cards", %{initial_state: state} do
@@ -339,10 +342,10 @@ defmodule TcgmWebApp.Game.GameLogicTest do
     action_args = %{"location" => "deck", "card" => card1}
     state_with_card_in_deck = GameLogic.insert_card(state, "player1", action_args)
 
-    assert Map.has_key?(state_with_card_in_deck.players["player1"]["deck"], "Card A") == true
-    assert Map.has_key?(state_with_card_in_deck.players["player1"]["deck"], "Card B") == true
-    assert Map.has_key?(state_with_card_in_deck.players["player1"]["deck"], "Card C") == true
-    assert Map.has_key?(state_with_card_in_deck.players["player1"]["deck"], "Card Y") == true
+    assert Enum.any?(state_with_card_in_deck.players["player1"]["deck"], fn card -> Map.has_key?(card, "Card A") end) == true
+    assert Enum.any?(state_with_card_in_deck.players["player1"]["deck"], fn card -> Map.has_key?(card, "Card B") end) == true
+    assert Enum.any?(state_with_card_in_deck.players["player1"]["deck"], fn card -> Map.has_key?(card, "Card C") end) == true
+    assert Enum.any?(state_with_card_in_deck.players["player1"]["deck"], fn card -> Map.has_key?(card, "Card Y") end) == true
   end
 
   test "set_turn set the turn state", %{initial_state: state} do

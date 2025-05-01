d7868f57ef460ea3e769e6c092d0e150442866c1
zacharie lacombe <zacharie.lacombe@epitech.eu>
2025-05-01 21:17:04 +0200

    🗃️  update card Type properties table

--- back/tcgm_web_app/priv/repo/migrations/20250226172103_make_new_cardtype_property_value_table.exs
+++ back/tcgm_web_app/priv/repo/migrations/20250226172103_make_new_cardtype_property_value_table.exs
@@ -9,6 +9,16 @@ defmodule TcgmWebApp.Repo.Migrations.MakeNewCardtypeProperty do
       add :value, :string, null: false
       add :variant, :string, null: false
       add :mutable, :boolean, null: false
+      add :font, :string, null: true
+      add :font_size, :integer, null: true
+      add :font_color, :string, null: true
+      add :position_x, :integer, null: false
+      add :position_y, :integer, null: false
+      add :rotation, :integer, null: false
+      add :scale_x, :integer, null: false
+      add :scale_y, :integer, null: false
+      add :border_width, :integer, null: false
+      add :border_color, :string, null: false
       add :border_radius, :string, null: false
       add :opacity, :int, null: false
       add :image, :string, null: true
@@ -20,18 +30,6 @@ defmodule TcgmWebApp.Repo.Migrations.MakeNewCardtypeProperty do
       add :image_scale_x, :integer, null: true
       add :image_scale_y, :integer, null: true
       add :image_opacity, :integer, null: true
-      add :font, :string, null: true
-      add :font_size, :integer, null: true
-      add :font_color, :string, null: true
-      add :position_x, :integer, null: false
-      add :position_y, :integer, null: false
-      add :rotation, :integer, null: false
-      add :scale_x, :integer, null: false
-      add :scale_y, :integer, null: false
-      add :border_width, :integer, null: false
-      add :border_color, :string, null: false
-      add :border_radius, :integer, null: false
-      add :opacity, :integer, null: false
       timestamps()
     end
   end

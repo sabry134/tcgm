f4862de1194ecc4003a62ca6a036480768160a29
Viro Neo <van-do.pham@epitech.eu>
2025-05-08 12:28:22 +0200

    :truck: cleaned up code

--- front/src/Theme.jsx
+++ front/src/Theme.jsx
@@ -2,17 +2,17 @@ import { createTheme } from '@mui/material/styles';
 import '@fontsource/libre-baskerville';
 
 const theme = createTheme({
-    palette: {
-      primary: {
-          main: '#7E4F00',
-          dark: '#653F00',
-          light: '#975F00',
-          contrastText: '#C4C4C4',
-      },
+  palette: {
+    primary: {
+      main: '#7E4F00',
+      dark: '#653F00',
+      light: '#975F00',
+      contrastText: '#C4C4C4',
     },
-    typography: {
-        fontFamily: `"Libre Baskerville", serif`,
-    }
+  },
+  typography: {
+    fontFamily: `"Libre Baskerville", serif`,
+  },
 });
 
 export default theme;

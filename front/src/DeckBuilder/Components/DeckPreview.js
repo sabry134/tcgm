9dae36d3871e2bcec97f40e0bb6ce55795f8224a
Viro Neo <van-do.pham@epitech.eu>
2025-06-25 11:53:12 +0200

    :art: :construction: Applied new layout on JoinRoom

--- front/src/DeckBuilder/Components/DeckPreview.js
+++ front/src/DeckBuilder/Components/DeckPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const DeckPreview = ({ deck, removeSingleCard, setHoveredCard, deckGroups }) => {
   const groupCards = (cards) => {

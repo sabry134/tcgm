5d4ab94f8aa29bdc0d02ef443ffd7905d90c24cf
Viro Neo <van-do.pham@epitech.eu>
2025-06-30 16:07:05 +0200

    :art: applied base layout on game main page

--- front/src/GameMainPage/GameMainPage.jsx
+++ front/src/GameMainPage/GameMainPage.jsx
@@ -1,4 +1,3 @@
-import { Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { BaseLayout } from "../Components/Layouts/BaseLayout";
